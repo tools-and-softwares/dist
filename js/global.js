73c3afd5f5e7c99909adcc4a2857cbdc87bd47b1
irahulsaini <irahulsaini01@gmail.com>
2022-09-03 11:45:53 +0530

    Create global.js

---
+++ js/global.js
@@ -0,0 +1,11 @@
+$.ajax({
+     url: "https://geolocation-db.com/jsonp",
+     jsonpCallback: "callback",
+     dataType: "jsonp",
+     success: function(location) {
+          $('.country').val(location.country_name);
+          $('.state').val(location.state);
+          $('.city').val(location.city);
+          geo = 1;
+     }
+});  
